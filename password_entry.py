d3360a319d7890e2e40f27fcad99a50f348076d8
Alfin00 <alfin.sabu@my.jcu.edu.au>
2019-08-27 10:26:04 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,7 @@
 """Alfin"""
+
+min_password = 5
+password = input('Enter Password of 5 Characters Min:')
+while len(password) < min_password:
+    password = input('Enter password of 5 characters:')
+print('*'*len(password))
